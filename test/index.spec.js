b04f90590737f7590babff0d81b8e321d1bd5844
Jinu Joseph Daniel <jinu.daniel@collygo.com>
2022-10-28 09:21:51 +0530

    first commit

---
+++ test/index.spec.js
@@ -0,0 +1,10 @@
+import {expect, jest, test} from '@jest/globals'; 
+
+function test2(a, b) {
+    return a+b;
+}
+describe("Test for testing", () => {
+    test("New test", () => {
+
+    }) 
+})  
